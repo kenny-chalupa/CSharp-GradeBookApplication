4527e9ba99f85f767fbddaa9fe23520c07153ef8
Kenny Chalupa <kechalup@microsoft.com>
2019-06-01 16:12:45 -0500

    Added Override of RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,18 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name) {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            char rankedLetter = 'F';
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Gradebook requires 5 Students for Ranked Grades");
+                
+            }
+
+
+            return rankedLetter;
+        }
     }
 }
